dbb8eb970313ee4a6e03ff6392237c5dc2982b0c
C:/Program Files/Git/ANASTASIA-2112 <anastasia.adamovich@yandex.by>
2022-12-17 12:57:43 +0300

    init: task

---
+++ freecodewards/adding-a-default-option-in-switch-statements.js
@@ -0,0 +1,29 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case "a":
+    answer ="apple";
+    break;
+ case "b":
+     answer ="bird";
+    break;
+ case "c":
+     answer ="cat";
+    break;
+case "d":
+     answer ="stuff";
+    break;
+  case 4:
+     answer ="stuff";
+    break;
+ default:
+    answer ="stuff";
+    break;   
+}
+return answer;
+  // Only change code above this line 
+
+}
+
+switchOfStuff(1);
